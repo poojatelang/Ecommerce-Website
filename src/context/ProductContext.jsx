968789e036c2ced4f74bec1f4f3577bd3783d1df
DrumkitReact <poojamtelang@gmail.com>
2024-05-28 13:18:52 +0530

    code complted

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
-import reducer from "../reducer/productReducer";
+import reducer from "./reducer/productReducer";
 
 const AppContext=createContext();
 
